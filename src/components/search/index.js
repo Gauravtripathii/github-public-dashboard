f4396066a999b20502bdac667145ad611b9d872d
Gauravtripathii <gauravtripathii7979@gmail.com>
2023-09-17 13:17:08 +0530

    search data get function

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./main.css";
 
+import { useState } from "react";
+
 function Search() {
+  const [profileName, setProfileName] = useState("");
+  const handleSubmit = () => {
+    console.log(profileName);
+    setProfileName("");
+  };
   return (
     <div className="search-container">
       <div className="input-box">
-        <input type="text" placeholder="Search for a github profile" />
-        <button>
-          <span class="material-symbols-outlined">search</span>
+        <input
+          type="text"
+          placeholder="Search for a github profile"
+          value={profileName}
+          onChange={(e) => setProfileName(e.target.value)}
+        />
+        <button onClick={handleSubmit}>
+          <span className="material-symbols-outlined">search</span>
         </button>
       </div>
     </div>
